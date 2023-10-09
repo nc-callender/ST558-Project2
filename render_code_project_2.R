1232d8876bd8402b5192c02243cb559188bee3a5
nc-callender <eddieandrockyarecats.gmail.com>
2023-10-09 15:51:13 -0400

    woking on df_print

--- render_code_project_2.R
+++ render_code_project_2.R
@@ -1,4 +1,4 @@
 rmarkdown:: render("C:/Users/Office/Desktop/NCSU Fall 2023/ST558-Project2/Project 2.Rmd",
                    output_file ="C:/Users/Office/Desktop/NCSU Fall 2023/ST558-Project2/README.md",
                    output_format = "github_document",
-                   output_options = list( toc=TRUE, df_print="paged"))
+                   output_options = list( toc=TRUE, df_print="tibble"))
