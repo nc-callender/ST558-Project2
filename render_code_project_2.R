c7199b4305334207b836b455d19454b787440783
nc-callender <eddieandrockyarecats.gmail.com>
2023-10-09 20:31:58 -0400

    ouput options

--- render_code_project_2.R
+++ render_code_project_2.R
@@ -1,4 +1,6 @@
 rmarkdown:: render("C:/Users/Office/Desktop/NCSU Fall 2023/ST558-Project2/Project 2.Rmd",
-                   output_file ="C:/Users/Office/Desktop/NCSU Fall 2023/ST558-Project2/README.md",
+                   output_file ="C:/Users/Office/Desktop/NCSU Fall 2023/ST558-Project2/README_files/README.md",
                    output_format = "github_document",
-                   output_options = list( toc=TRUE, df_print="kable"))
+                   output_options = (toc=TRUE))
+
+
